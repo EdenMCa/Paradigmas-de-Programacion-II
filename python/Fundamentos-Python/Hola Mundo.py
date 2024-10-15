e78bd5a27a0e63883da3b04cc82ed3b4f3aabd9c
Eden Casarrubia desde Laptop <casarrubiaeden@gmail.com>
2024-10-15 01:19:07 -0600

    Se crearon los archivos "Hola Mundo" y "Uso_de_variables". 09-10-2024

--- python/Fundamentos-Python/Hola Mundo.py
+++ python/Fundamentos-Python/Hola Mundo.py
@@ -4,6 +4,11 @@ Descripción: Primer "Hola mundo" en Python
 Fecha: 09-10-2024.
 """
 
+# Se utilizan tres sentencias print para mostrar diferentes variaciones de "Hola Mundo".
+# La primera línea imprime "Hola Mundo" en español.
+# La segunda línea imprime 'Hola Mundo' (con comillas simples).
+# La tercera línea imprime "Hello World".
+
 print("Hola Mundo")
 print('Hola Mundo')
 print("Hello World")
