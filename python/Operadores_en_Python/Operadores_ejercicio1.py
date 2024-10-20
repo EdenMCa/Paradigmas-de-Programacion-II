0950b5f96f5a4bcae68df99c454d30553a5bad75
Eden Casarrubia desde Laptop <casarrubiaeden@gmail.com>
2024-10-20 10:18:16 -0600

    Se realizó el programa 'Operadores_ejercicio1' y se subió debidamente comentado. 20 - 10 - 2024

--- python/Operadores_en_Python/Operadores_ejercicio1.py
+++ python/Operadores_en_Python/Operadores_ejercicio1.py
@@ -1,18 +1,20 @@
 """
 Nombre: Mendoza Casarrubia Rosendo Eden
 Fecha: 18 - 10 - 2024
-Descripción:
+Descripción: Programa que determina si el gasto aplica para una bonificación de buen fin.
 """
 
-print("\t BONIFICACIÓN BUEN FIN")
-
+print("\t *** BONIFICACIÓN BUEN FIN ***")
 gasto = float(input("¿Qué cantidad gastó? "))
+# Solicita al usuario la cantidad gastada y la convierte a tipo float para realizar cálculos numéricos
 cantidad = gasto > 5000
-print()
+# Verifica si la cantidad gastada es mayor a 5000 y almacena el resultado.
 MSI = (input("¿La compra fue a MSI? "))
+# Pregunta al usuario si la compra fue a meses sin intereses (MSI) y almacena la respuesta
 MSI = MSI.lower() == "si"
-print(MSI)
+# Convierte la respuesta del usuario a minúsculas y verifica si es "si", almacenando el resultado como un booleano
 
-print(f"Aplica bonificación de Buen Fin: {cantidad or MSI}")
-
-
+print(f"Aplica bonificación de Buen Fin: {cantidad and not MSI} ")
+# Aplicará la bonificación si la cantidad es mayor a 5000 y no fue a MSI.
+# and not se traduce literal como "y no". La expresión and not se utiliza para verificar que una cantidad es mayor a 5000 y que no se pagó a meses sin intereses.
+# Si cualquiera de esas condiciones no se cumple, el resultado será False.
