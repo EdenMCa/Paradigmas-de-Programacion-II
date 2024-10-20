9feaaaea39cef4597236ddb9e3825470e2a14685
Eden Casarrubia desde Laptop <casarrubiaeden@gmail.com>
2024-10-20 09:12:06 -0600

    Se subió el programa 'Entrada_conversiones' debidamente comentado. 20 - 10 - 2024

--- python/Fundamentos-Python/Entrada_conversiones.py
+++ python/Fundamentos-Python/Entrada_conversiones.py
@@ -1,11 +1,13 @@
 '''
-Nombre:
-Fecha:
-Descripción:
-Conversión de cadenas a int, float y boolean mediante la interacción con consola.
+Nombre: Mendoza Casarrubia Rosendo Eden
+Fecha: 20 - 10 - 2024
+Descripción: Conversión de cadenas a int, float y boolean mediante la interacción con consola.
 '''
 
 # Comentar sobre las funciones anidadas.
+# El término "anidado" a menudo se asocia con una estructura de funciones dentro de funciones
+# (como definir una función dentro de otra). En este caso, aunque estamos utilizando input() dentro de int(),
+# el enfoque es más sobre composición de funciones que sobre anidamiento en sí.
 print("****   Datos de los alumnos    *****")
 nombre = input("Ingresa el nombre: ")
 semestre = int(input("Ingresa el no. de semestre: "))
@@ -18,5 +20,10 @@ es_mujer = es_mujer.lower() == "si"
 
 # Se imprimen los datos del alumno.
 # Comentar  qué es lo que realiza {promedio:.2f} probando con números diferentes.
+# La expresión {promedio:.2f} es una forma de formateo de cadenas en Python que permite controlar cómo se presenta un número decimal en la salida.
+# :.2f Esto es un especificador de formato que indica cómo debe presentarse el valor.
+#: Introduce la parte de formato de la especificación.
+# .2: Indica que se debe mostrar exactamente 2 dígitos después del punto decimal, si nosotros cambiamos el número por cualquier otro solo va a modificar cuantos número apareceran despúes del punto.
+# f: Indica que se trata de un número en formato de punto flotante (float).
 print()
 print(f"El alumno {nombre} cursa el {semestre} semestre con un promedio de {promedio:.2f}. Además, es mujer: {es_mujer}.")
