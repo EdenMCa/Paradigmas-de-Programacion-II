222ae49cbf1943c8b6904b5b19e8b5ba8bd3532d
Eden Casarrubia desde Laptop <casarrubiaeden@gmail.com>
2024-10-26 14:55:28 -0600

    El programa Operadores_ejercicio1, se ha comentado y se ha subido correctamente. 26 - 10 - 2024

--- python/Operadores_en_Python/Operadores_ejercicio1.py
+++ python/Operadores_en_Python/Operadores_ejercicio1.py
@@ -12,9 +12,8 @@ cantidad = gasto > 5000
 MSI = (input("¿La compra fue a MSI? "))
 # Pregunta al usuario si la compra fue a meses sin intereses (MSI) y almacena la respuesta
 MSI = MSI.lower() == "si"
-# Convierte la respuesta del usuario a minúsculas y verifica si es "si", almacenando el resultado como un booleano
+# Convierte la respuesta del usuario a minúsculas y verifica si es "sí", almacenando el resultado como un booleano
 
-print(f"Aplica bonificación de Buen Fin: {cantidad and not MSI} ")
-# Aplicará la bonificación si la cantidad es mayor a 5000 y no fue a MSI.
-# and not se traduce literal como "y no". La expresión and not se utiliza para verificar que una cantidad es mayor a 5000 y que no se pagó a meses sin intereses.
+print(f"Aplica bonificación de Buen Fin: {cantidad and MSI} ")
+# Aplicará la bonificación si la cantidad es mayor a 5000 y fue a MSI.
 # Si cualquiera de esas condiciones no se cumple, el resultado será False.
